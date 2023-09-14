f5bd0af9bd10021ba40f363a1511be067c050a3b
subu131988 <144934800+subu131988@users.noreply.github.com>
2023-09-14 11:13:52 +0100

    Update streamlit_app.py
    According to the selection the table changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 streamlit.dataframe(my_fruit_list)
 
 # Display the table on the page.
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
